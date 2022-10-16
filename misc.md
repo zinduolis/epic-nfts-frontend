54bcaa235774baa6f682d1682b8214a37037764b
Your Name <you@example.com>
2022-10-16 21:51:46 +1000

    Updated logic to monitor for events

---
+++ misc.md
@@ -0,0 +1,7 @@
+Let's say you want to change your contract. You'd need to do 3 things:
+
+We need to deploy it again.
+
+We need to update the contract address on our frontend.
+
+We need to update the abi file on our frontend.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import { EtherscanProvider } from "@ethersproject/providers";
+import { ethers } from "ethers";
+import myEpicNft from './utils/MyEpicNFT.json';
 
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0x16a2E4eB093d3460Ae518ec88A4B6CE7992DdD48";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -26,6 +29,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account: ", account);
       setCurrentAccount(account);
+      setupEventListener();
     } else {
       console.log("No authorized account detected");
     }
@@ -42,16 +46,72 @@ const App = () => {
       const accounts = await ethereum.request({ method: "eth_requestAccounts"});
       console.log("Connected with account: ", accounts[0]);
       setCurrentAccount(accounts[0]);
+      setupEventListener();
     } catch (error) {
       console.log(error); 
     }
   }
 
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take max of 10 min to show up on Rarible. Here's the link: https://testnet.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`);
+        });
+
+        console.log("Setup event Listener!");
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const askContractToMintNft = async () => {
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect Your Wallet
     </button>
   );
+
+  const renderMintUI = () => {
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  }
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -65,13 +125,7 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
-              Mint NFT
-            </button>
-          )}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
